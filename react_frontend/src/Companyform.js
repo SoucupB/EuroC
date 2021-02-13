dba12ebd455ab2682c68db0bbb50270c89bcac82
Bogdan Soucup <soucup.bogdan@gmail.com>
2021-02-13 18:34:09 +0200

    Add email change

---
+++ react_frontend/src/Companyform.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Companyform() {
+  return (
+    <div>
+      Ana are mere
+    </div>
+  )
+}
--- react_frontend/src/Loginbox.js
+++ react_frontend/src/Loginbox.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './css/Loginpage.css';
+import _uniqueId from 'lodash/uniqueId';
 
 const boxClass = "loginbox"
 const labelClass = "label-loginbox"
@@ -13,15 +14,48 @@ const absolute = "inside-objects"
 const middleText = "text-vertical-center"
 const buttonPoss = "button-class-position"
 const registerClass = "register"
+import {Redirect} from 'react-router';
 
 export default function Loginbox({buttonLogin, buttonRegister}) {
+  let emailId;
+  let parolaId;
+
+  const getEmailId = function() {
+    if(emailId) {
+      return emailId;
+    }
+    emailId = _uniqueId('')
+    return emailId
+  }
+
+  const getParolaId = function() {
+    if(parolaId) {
+      return parolaId;
+    }
+    parolaId = _uniqueId('')
+    return parolaId
+  }
+
+  const getEmail = function() {
+    return document.getElementById(getEmailId()).value
+  }
+
+  const getPasword = function() {
+    return document.getElementById(getParolaId()).value
+  }
+
+  const loginFunction = function() {
+    console.log(getEmail(), getPasword());
+    return <Redirect  to="/posts/" />
+  };
+
   return (
     <div className = {boxClass}>
       <label className = {[absolute, labelClass, labelUsername].join(' ')}>Email</label>
-      <input className = {[absolute, classInput, inputEmail].join(' ')} type="email"/>
+      <input id = {getEmailId()} className = {[absolute, classInput, inputEmail].join(' ')} type="email"/>
       <label className = {[absolute, labelClass, labelParola].join(' ')}>Parola</label>
-      <input className = {[absolute, classInput, inputPassword].join(' ')} type="password"/>
-      <div className = {[absolute, buttonClass, buttonPoss].join(' ')}><p className={middleText}>{buttonLogin}</p></div>
+      <input id = {getParolaId()} className = {[absolute, classInput, inputPassword].join(' ')} type="password"/>
+      <div onClick = {loginFunction} className = {[absolute, buttonClass, buttonPoss].join(' ')}><p className={middleText}>{buttonLogin}</p></div>
       <div className = {[absolute, buttonClass, buttonPoss, registerClass].join(' ')}><p className={middleText}>{buttonRegister}</p></div>
     </div>
   )
